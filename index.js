bd881bc2f370499cc1f4de533d28281a565af2db
john <johnmaluki58@gmail.com>
2023-06-14 17:24:49 +0300

    Complete assignment on scope lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const leastFavoriteCustomer = "doe";
+var customerName = "bob";
+var bestCustomer;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(value) {
+  bestCustomer = value;
+}
+
+function changeLeastFavoriteCustomer(value) {
+  leastFavoriteCustomer = value;
+}
